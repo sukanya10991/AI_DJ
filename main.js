a6a2b2bffec5d385f69ae4657d46a613a1433415
sukanya10991 <78801848+sukanya10991@users.noreply.github.com>
2021-10-24 08:50:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song = "";
 song1 = "";
+leftwristx = 0;
+leftwristy = 0;
+rightwristx = 0;
+rightwristy = 0;
 
 function preload() {
 
@@ -12,10 +16,31 @@ function setup() {
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.hide();
+    video.hide();    
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 500, 400);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftwristx = results[0].pose.leftWrist.x;
+        leftwristy = results[0].pose.leftWrist.y;
+        console.log("Left wrist x=" + leftwristx + "Left wrist y=" + leftwristy);
+
+        rightwristx = results[0].pose.rightWrist.x;
+        rightwristy = results[0].pose.rightWrist.y;
+        console.log("Right wrist x=" + rightwristx + "Right wrist y=" + rightwristy);
+    }
 }
 
